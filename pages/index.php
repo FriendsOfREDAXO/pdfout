e2b730ad25bba36f1c80aa31bd2d5516f0c362fb
Thomas Skerbis <thomas.skerbis@mac.com>
2018-07-19 13:40:05 +0200

    wird nicht benötigt

--- pages/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/** @var rex_addon $this */
-echo rex_view::title($this->i18n('title')); 
-$subpage = rex_be_controller::getCurrentPagePart(2);
-rex_be_controller::includeCurrentPageSubPath();
