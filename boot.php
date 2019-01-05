c312a85ff844eb376def6e093964f6c671512189
Simon Krull <simondotunix@gmail.com>
2019-01-05 21:26:37 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -2,24 +2,24 @@
 
 require_once $this->getPath('vendor/dompdf/src/'.'Autoloader.php');
 
-$print_pdftest = rex_request('pdftest', 'int');
-if ($print_pdftest==1) {
+if (rex::isBackend() && rex::getUser()) {
+    $print_pdftest = rex_request('pdftest', 'int');
+    if ($print_pdftest == 1) {
+        rex_response::cleanOutputBuffers(); // OutputBuffer leeren
+        $file = rex_file::get(rex_path::addon('pdfout', 'README.md'));
+        $readmeHtml = rex_markdown::factory()->parse($file);
+        $art_pdf_name = 'pdftest'; // Dateiname
+        $dompdf = new Dompdf\Dompdf();
+        $dompdf->loadHtml($readmeHtml);
+        // Optionen festlegen
+        $dompdf->set_option('defaultFont', 'Helvetica');
+        $dompdf->set_option('dpi', '100');
+        // Rendern des PDF
+        $dompdf->render();
+        // Ausliefern des PDF
+        header('Content-Type: application/pdf');
+        $dompdf->stream($art_pdf_name, array('Attachment' => false)); // bei true wird Download erzwungen
+        die();
 
-    rex_response::cleanOutputBuffers(); // OutputBuffer leeren
-    $file = rex_file::get(rex_path::addon('pdfout','README.md'));
-    $readmeHtml = rex_markdown::factory()->parse($file);
-    $art_pdf_name =  'pdftest'; // Dateiname
-    $dompdf = new Dompdf\Dompdf();
-    $dompdf->loadHtml($readmeHtml);
-    // Optionen festlegen
-    $dompdf->set_option('defaultFont', 'Helvetica');
-    $dompdf->set_option('dpi', '100');
-    // Rendern des PDF
-    $dompdf->render();
-    // Ausliefern des PDF
-    header('Content-Type: application/pdf');
-    $dompdf->stream($art_pdf_name ,array('Attachment'=>false)); // bei true wird Download erzwungen
-    die();
+    }
 }
-
-
