38514c23662f1c545f0f9353794b0a05942fe9ee
Simon Krull <simondotunix@gmail.com>
2019-01-05 13:55:31 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -6,11 +6,9 @@ $print_pdftest = rex_request('pdftest', 'int');
 if ($print_pdftest==1) {
 
     rex_response::cleanOutputBuffers(); // OutputBuffer leeren
-    // Artikel laden oder alternativ ein Template
     $file = rex_file::get(rex_path::addon('pdfout','README.md'));
     $pdfcontent = rex_markdown::factory()->parse($file);
     $art_pdf_name =  'pdftest'; // Dateiname
-    header('Content-Type: application/pdf');
     $dompdf = new Dompdf\Dompdf();
     $dompdf->loadHtml($pdfcontent);
     // Optionen festlegen
@@ -19,8 +17,9 @@ if ($print_pdftest==1) {
     // Rendern des PDF
     $dompdf->render();
     // Ausliefern des PDF
+    header('Content-Type: application/pdf');
     $dompdf->stream($art_pdf_name ,array('Attachment'=>false)); // bei true wird Download erzwungen
     die();
 }
 
-?>
+
