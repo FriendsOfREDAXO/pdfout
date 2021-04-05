06837547d6ec3c2efbfe57e2bbbedec80266c972
Thomas Skerbis <thomas.skerbis@mac.com>
2021-04-05 21:19:24 +0200

    cs and var rename

--- lib/pdfout.php
+++ lib/pdfout.php
@@ -1,25 +1,26 @@
 <?php
 use Dompdf\Dompdf;
+
 class PdfOut extends Dompdf
 {
- public static function sendPdf($name = 'pdf_file', $html = '', $orientation = 'portrait', $defaultFont ='Courier', $attachment = false, $remoteFiles = true)
- {
-  rex_response::cleanOutputBuffers(); // OutputBuffer leeren
-        $dompdf = new self();
-        $dompdf->loadHtml($html);
+    public static function sendPdf($name = 'pdf_file', $html = '', $orientation = 'portrait', $defaultFont = 'Courier', $attachment = false, $remoteFiles = true)
+    {
+        rex_response::cleanOutputBuffers(); // OutputBuffer leeren
+        $pdf = new self();
+        $pdf->loadHtml($html);
         // Optionen festlegen
-        $dompdf->set_option('isRemoteEnabled', $remoteFiles);
-        $dompdf->set_option('chroot', mb_substr(rex_path::base(), 0, -1));
-        $dompdf->set_option('font_cache', rex_path::addonCache('pdfout', 'fonts'));
-        $dompdf->set_option('defaultFont', $defaultFont);
-        $dompdf->setPaper('A4', $orientation);
-        $dompdf->set_option('dpi', '100');
+        $pdf->set_option('isRemoteEnabled', $remoteFiles);
+        $pdf->set_option('chroot', mb_substr(rex_path::base(), 0, -1));
+        $pdf->set_option('font_cache', rex_path::addonCache('pdfout', 'fonts'));
+        $pdf->set_option('defaultFont', $defaultFont);
+        $pdf->setPaper('A4', $orientation);
+        $pdf->set_option('dpi', '100');
         // Rendern des PDF
-        $dompdf->render();
+        $pdf->render();
         // Ausliefern des PDF
         header('Content-Type: application/pdf');
-        $dompdf->stream($name ,array('Attachment'=>false));
-   die();
- }
- 
+        $pdf->stream(rex_string::normalize($name), array('Attachment' => false));
+        die();
+    }
+
 }
