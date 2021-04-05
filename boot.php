99213235138ccd2d75d1466914d811574969be7f
Thomas Skerbis <thomas.skerbis@mac.com>
2021-04-05 17:30:10 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,6 +1,7 @@
 <?php
 $addon = rex_addon::get('pdfout');
 rex_dir::create($addon->getCachePath());
+rex_dir::create(rex_path::addonCache('pdfout', 'fonts'));
 
 require_once $this->getPath('vendor/'.'autoload.php');
 
