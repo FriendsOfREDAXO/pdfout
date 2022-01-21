f16e444749535159337e43c086a35eb8b49e8021
Thomas Skerbis <thomas.skerbis@mac.com>
2022-01-21 16:10:47 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -8,7 +8,6 @@ require_once $this->getPath('vendor/'.'autoload.php');
 if (rex::isBackend() && rex::getUser()) {
     $print_pdftest = rex_request('pdftest', 'int');
     if ($print_pdftest == 1) {
-        rex_response::cleanOutputBuffers(); // OutputBuffer leeren
         $file = rex_file::get(rex_path::addon('pdfout', 'README.md'));
         $readmeHtml = '<style>body {font-family: DejaVu Sans; }</style>'.rex_markdown::factory()->parse($file);
         PdfOut::sendPdf('readme',$readmeHtml);
