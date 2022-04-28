0216f32ee5bee8e7c84c80d142d726b428ecd090
Daniel Springer <dsp.springer@googlemail.com>
2022-04-28 17:23:29 +0200

    PDF auf dem Server speichern
    Mit dem Parameter "$saveToPath" kann man einen Pfad definieren, auf welchem die generierte PDF auf dem Server gespeichert wird.
    
    https://github.com/FriendsOfREDAXO/pdfout/issues/49

--- lib/pdfout.php
+++ lib/pdfout.php
@@ -3,15 +3,15 @@ use Dompdf\Dompdf;
 
 class PdfOut extends Dompdf
 {
-    public static function sendPdf($name = 'pdf_file', $html = '', $orientation = 'portrait', $defaultFont = 'Courier', $attachment = false, $remoteFiles = true)
+    public static function sendPdf($name = 'pdf_file', $html = '', $orientation = 'portrait', $defaultFont = 'Courier', $attachment = false, $remoteFiles = true, $saveToPath = '')
     {
         rex_response::cleanOutputBuffers(); // OutputBuffer leeren
         $pdf = new self();
         $pdf->loadHtml($html);
-        
+
         // Optionen festlegen
         $options = $pdf->getOptions();
-	$options->setChroot(rex_path::frontend());
+        $options->setChroot(rex_path::frontend());
         $options->setDefaultFont($defaultFont);
         $options->setDpi(100);
         $options->setFontCache(rex_path::addonCache('pdfout', 'fonts'));
@@ -19,13 +19,20 @@ class PdfOut extends Dompdf
         $pdf->setOptions($options);
         $pdf->setPaper('A4', $orientation);
 
-		// Rendern des PDF
+        // Rendern des PDF
         $pdf->render();
-        // Ausliefern des PDF
-        header('Content-Type: application/pdf');
-        $pdf->stream(rex_string::normalize($name), array('Attachment' => $attachment));
+
+        // Ausliefern des PDF - entweder anzeigen der File oder auf Server speichern
+        if($saveToPath == '') {
+            header('Content-Type: application/pdf');
+            $pdf->stream(rex_string::normalize($name), array('Attachment' => $attachment));
+        } else {
+            file_put_contents($saveToPath.$name.'.pdf', $pdf->output());
+        }
+
         die();
     }
+
     public static function viewer($file = '')
     {
         if ($file!='')
