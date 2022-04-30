1127b98415ad393ebe65a0b36c87a19273885c4a
Daniel Springer <daniel.springer@medienfeuer.de>
2022-04-30 18:28:16 +0200

    PDF auf dem Server speichern (#50)
    * PDF auf dem Server speichern
    
    Mit dem Parameter "$saveToPath" kann man einen Pfad definieren, auf welchem die generierte PDF auf dem Server gespeichert wird.
    
    https://github.com/FriendsOfREDAXO/pdfout/issues/49
    
    * Update: PDF auf dem Server speichern
    
    Korrigiert den PR https://github.com/FriendsOfREDAXO/pdfout/pull/50
    - die() nur wenn PDF angezeigt wird, nicht wenn die PDF gespeichert wird
    - rex_string::normalize($filename)
    
    * Update: PDF auf dem Server speichern
    
    Korrigiert den PR https://github.com/FriendsOfREDAXO/pdfout/pull/50
    - Umstellung auf rex_file::put()

--- lib/pdfout.php
+++ lib/pdfout.php
@@ -3,15 +3,15 @@ use Dompdf\Dompdf;
 
 class PdfOut extends Dompdf
 {
-    public static function sendPdf($name = 'pdf_file', $html = '', $orientation = 'portrait', $defaultFont = 'Courier', $attachment = false, $remoteFiles = true)
+    public static function sendPdf($name = 'pdf_file', $html = '', $orientation = 'portrait', $defaultFont = 'Courier', $attachment = false, $remoteFiles = true, $saveToPath = '')
     {
         rex_response::cleanOutputBuffers(); // OutputBuffer leeren
         $pdf = new self();
         $pdf->loadHtml($html);
-        
+
         // Optionen festlegen
         $options = $pdf->getOptions();
-	$options->setChroot(rex_path::frontend());
+        $options->setChroot(rex_path::frontend());
         $options->setDefaultFont($defaultFont);
         $options->setDpi(100);
         $options->setFontCache(rex_path::addonCache('pdfout', 'fonts'));
@@ -19,13 +19,20 @@ class PdfOut extends Dompdf
         $pdf->setOptions($options);
         $pdf->setPaper('A4', $orientation);
 
-		// Rendern des PDF
+        // Rendern des PDF
         $pdf->render();
-        // Ausliefern des PDF
-        header('Content-Type: application/pdf');
-        $pdf->stream(rex_string::normalize($name), array('Attachment' => $attachment));
-        die();
+
+        // Ausliefern des PDF - entweder anzeigen der File oder auf Server speichern
+        if($saveToPath == '') {
+            header('Content-Type: application/pdf');
+            $pdf->stream(rex_string::normalize($name), array('Attachment' => $attachment));
+            die();
+        } else {
+            rex_file::put($saveToPath.rex_string::normalize($name).'.pdf', $pdf->output());
+        }
+
     }
+
     public static function viewer($file = '')
     {
         if ($file!='')
