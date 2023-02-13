54e3e563d4762d84de6d994418aebde931a06a59
Thomas Skerbis <skerbis@klxm.de>
2023-02-13 18:11:50 +0100

    added ->setRemoteFiles($remoteFiles) to old function

--- lib/pdfout.php
+++ lib/pdfout.php
@@ -120,6 +120,7 @@ class PdfOut extends Dompdf
             ->setSaveAndSend(false)
             ->setSaveToPath($saveToPath)
             ->setDpi(100);
+            ->setRemoteFiles($remoteFiles)
         $pdf->run();
     }
 
