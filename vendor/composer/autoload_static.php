682187b5ce212db047ec177ae940323d1305c295
Thomas Skerbis <thomas.skerbis@mac.com>
2024-05-24 20:45:21 +0200

    vendor3

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -29,7 +29,7 @@ class ComposerStaticInitecd446c0d55531961cf4f51c51253933
     public static $prefixDirsPsr4 = array (
         'Svg\\' => 
         array (
-            0 => __DIR__ . '/..' . '/phenx/php-svg-lib/src/Svg',
+            0 => __DIR__ . '/..' . '/dompdf/php-svg-lib/src/Svg',
         ),
         'Sabberworm\\CSS\\' => 
         array (
@@ -41,7 +41,7 @@ class ComposerStaticInitecd446c0d55531961cf4f51c51253933
         ),
         'FontLib\\' => 
         array (
-            0 => __DIR__ . '/..' . '/phenx/php-font-lib/src/FontLib',
+            0 => __DIR__ . '/..' . '/dompdf/php-font-lib/src/FontLib',
         ),
         'Dompdf\\' => 
         array (
